ada6d81c33388e7d513bf2758f12cb5ad55cdd7e
Freddie Line <freddieline@hotmail.com>
2024-08-10 15:32:59 +0100

    fix: update data

--- src/components/summary.tsx
+++ src/components/summary.tsx
@@ -47,10 +47,14 @@ export const Summary: React.FC<InputProps> = ({ selectedMeals }) => {
               <b>Your daily footprint:</b> {getDailyEmissions()} kg CO2e
             </p>
             <p>
-              <b>UK average daily footprint:</b> 18 tons CO2e
-            </p>
-            <p>
-              <b>UK 2030 footprint target (pp):</b> 2.5 tons CO2e
+              <b>UK average daily footprint:</b> *27 kg CO2e
+              </p>
+              <p>
+                <b>UK 2030 daily footprint target (pp):</b> **12 tons CO2e
+              </p>
+              <p>
+              * based on an estimated <a target="_blank" href="https://www.carbonindependent.org/23.html">10 tons CO2e pp per year</a> 
+              <br/>** based on a <a target="_blank" href="https://www.theccc.org.uk/publication/progress-in-reducing-emissions-2024-report-to-parliament/">68% reduction in C02e emissions by 2030 compared to 1990s levels</a>
             </p>
           </>
         </>
